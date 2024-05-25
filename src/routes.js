ef8791c48a8b1a0f74f1dfe747236adcea297bd9
Henrique Balmant <henrique.balmant@gmail.com>
2024-05-25 02:40:57 -0300

    feat: add delete route

--- src/routes.js
+++ src/routes.js
@@ -83,4 +83,21 @@ export const routes = [
       return res.writeHead(204).end()
     }
   },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end(JSON.stringify({ message: "no record found" }))
+      }
+
+      database.delete('tasks', id)
+
+      return res.writeHead(204).end()
+    }
+  },
 ];
